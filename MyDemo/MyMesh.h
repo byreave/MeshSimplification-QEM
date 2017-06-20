17bfc60f5a29d5a6bcd44dfc084c9be8aeb3b1c9
byreave <654297651@qq.com>
2017-06-20 17:14:58 +0800

    Third commit

--- MyDemo/MyMesh.h
+++ MyDemo/MyMesh.h
@@ -13,6 +13,7 @@
 #include "Parser\parser.h"
 #include "Eigen\Dense"
 #include <vector>
+#include <queue>
 #ifndef M_PI
 #define M_PI 3.141592653589793238
 #endif
@@ -117,22 +118,28 @@ namespace MeshLib
 	class ValidPair
 	{
 	public:
-		ValidPair(CVertex * v1, CVertex * v2, bool isedge = false)
+		ValidPair(CVertex * v1, CVertex * v2, bool isedge = true)
 		{
 			m_vertex[0] = v1;
 			m_vertex[1] = v2;
 			is_edge = isedge;
 		}
 		~ValidPair();
-		bool operator < (const ValidPair & vp)
+		friend bool operator < (const ValidPair & vp1, const ValidPair & vp2)
 		{
-			return cost < vp.cost;
+			return vp1.cost < vp2.cost;
+		}
+		friend bool operator > (const ValidPair & vp1, const ValidPair & vp2)
+		{
+			return vp1.cost > vp2.cost;
 		}
 		CVertex * & operator [] (int i)
 		{
 			assert(i > -1 && i < 2);
 			return m_vertex[i];
 		}
+		void setCost(double co) { cost = co; }
+		bool isEdge(){ return is_edge; }
 	private:
 		CVertex * m_vertex[2];
 		double cost;
@@ -170,6 +177,8 @@ namespace MeshLib
 		MatrixXd compute_error(V * pV);
 		double compute_cost(MatrixXd v, MatrixXd mQ);
 		MatrixXd after_contraction(ValidPair vp);
+		void contract_valid_pair(ValidPair vp);
+		void QEM(double threshold, int times);
 	};
 
 	typedef MyMesh<CMyVertex, CMyEdge, CMyFace, CMyHalfEdge> CMyMesh;
@@ -249,7 +258,7 @@ namespace MeshLib
 	vector<F *> MyMesh<V, E, F, H>::faces_around_vertex(V * pV)
 	{
 		H * pH = pV->halfedge();
-		H * pH_next = pH->next()->sym();
+		H * pH_next = pH->he_next()->he_sym();
 		vector<F *> facesVec;
 		facesVec.push_back(pH->face());
 		//traverse all faces, pV->halfedge() is a most ccw in-coming halfedge
@@ -257,7 +266,7 @@ namespace MeshLib
 		while (pH_next != NULL && pH_next != pH)
 		{
 			facesVec.push_back(pH_next->face());
-			pH_next = pH_next->next()->sym();
+			pH_next = pH_next->he_next()->he_sym();
 		}
 		return facesVec;
 	}
@@ -319,7 +328,6 @@ namespace MeshLib
 
 	/*
 	returns the position (x, y, z, 1) of a vertex after contraction
-	param 1 is the Q matrix of v1 + v2
 	*/
 	template<typename V, typename E, typename F, typename H>
 	MatrixXd MyMesh<V, E, F, H>::after_contraction(ValidPair vp)
@@ -374,6 +382,185 @@ namespace MeshLib
 				cout << "error in k! \n";
 				return NULL;
 			}
+		}
+	}
+	template<typename V, typename E, typename F, typename H>
+	void MyMesh<V, E, F, H>::contract_valid_pair(ValidPair vp)
+	{
+		MatrixXd v = after_contraction(vp); //get position after contraction
+		vp[0]->point()[0] = v(0, 0);
+		vp[0]->point()[1] = v(1, 0);
+		vp[0]->point()[2] = v(2, 0);
+		if (vp.isEdge())
+		{
+			E * e = vertexEdge(vp[0], vp[1]);
+			
+			//find the halfedge with source being vp[0] and target being vp[1]
+			H * h;
+			if (e->halfedges(0)->source() == vp[0])
+				h = e->halfedges(0);
+			else if (e->halfedges(1) != NULL)
+				h = e->halfedges(1);
+			else //former operation makes sure there is a halfedge vp[0]->vp[1]
+			{
+				cout << "error in halfedge\n";
+				return;
+			}
+			H * h_prev = h->he_prev();
+			H * h_next = h->he_next();
+
+			//remove vp[1]
+			//m_edges in vp[1] to vp[0]
+			if (vp[0]->id() > vp[1]->id())
+				vp[0]->edges().remove(e);
+			H * hvp1 = vp[1]->halfedge();
+			H * hvp1_next = hvp1->he_next()->he_sym();
+			if (hvp1->source() != vp[0] && hvp1->source() != h_next->target())
+			{
+				if (h->he_sym() == NULL && hvp1->source()->id() < vp[0]->id()) //boundary
+					vp[0]->edges().insert(hvp1->edge());
+				if (h->he_sym() != NULL && hvp1->source() != h->he_sym()->he_next()->target() && hvp1->source()->id() < vp[0]->id())
+					vp[0]->edges().insert(hvp1->edge());
+			}
+			while (hvp1_next != NULL && hvp1_next != hvp1)
+			{
+				if (hvp1_next->source() != vp[0] && hvp1_next->source() != h_next->target())
+				{
+					if (h->he_sym() == NULL && hvp1_next->source()->id() < vp[0]->id()) //boundary
+						vp[0]->edges().insert(hvp1_next->edge());
+					if (h->he_sym() != NULL && hvp1_next->source() != h->he_sym()->he_next()->target() && hvp1_next->source()->id() < vp[0]->id())
+						vp[0]->edges().insert(hvp1_next->edge());
+				}
+				hvp1_next = hvp1_next->he_next()->he_sym();
+			}
+
+			//set all in and out halfedges of vp[1] to vp[0]
+			H * h_in_vp1 = vp[1]->halfedge();
+			H * h_out_vp1 = h_in_vp1->he_sym();
+			H * h_out_vp1_next = h_out_vp1->he_next()->he_sym();
+			H * h_in_vp1_next = h_in_vp1->he_next()->he_sym();
+			h_in_vp1->target() = vp[0];
+			while (he_in_vp1_next != he_in_vp1 && he_in_vp1_next != NULL)
+			{
+				h_in_vp1_next->target() = vp[0];
+				h_in_vp1_next = h_in_vp1_next->he_next()->he_sym();
+			}
+			he_out_vp1->source() = vp[0];
+			while (he_out_vp1_next != he_out_vp1 && he_out_vp1_next != NULL)
+			{
+				h_out_vp1_next->source() = vp[0];
+				h_out_vp1_next = h_out_vp1_next->he_next()->he_sym();
+			}
+			std::map<int, V*>::iterator viter = m_map_vert.find(vp[1]->id());
+			if (viter != m_map_vert.end())
+			{
+				m_map_vert.erase(viter);
+			}
+			m_verts.remove(vp[1]);//delete vp[1];
+
+			//edges
+			if (h_prev->edge()->halfedge(0) == he_prev)
+				h_prev->edge()->halfedge(0) = h_next->he_sym();
+			else
+				h_prev->edge()->halfedge(1) = h_next->he_sym();
+			m_edges.remove(h_next->edge());
+			delete h_next->edge();
+			delete h_prev;
+			delete h_next;
+			h_next = NULL;
+			h_prev = NULL;
+
+			if (h->he_sym() != NULL) // edge for contraction isn't on the boundary
+			{
+				h = h->he_sym();
+				h_prev = h->he_prev();
+				h_next = h->he_next();
+				//edges same as above
+				if (h_next->edge()->halfedge(0) == he_next)
+					h_next->edge()->halfedge(0) = h_prev->he_sym();
+				else
+					h_next->edge()->halfedge(1) = h_prev->he_sym();
+				m_edges.remove(h_prev->edge());
+				delete h_prev->edge();
+				delete h_next;
+				delete h_prev;
+				h_prev = NULL;
+				h_next = NULL;
+			}
+
+			//delete faces beside e
+			std::map<int, F *>::iterator fiter = m_map_face.find(h->face()->id());
+			if (fiter != m_map_face.end())
+			{
+				m_map_face.erase(fiter);
+			}
+			m_faces.remove(h->face());
+			
+			h = h->sym();
+			std::map<int, F *>::iterator fiter = m_map_face.find(h->face()->id());
+			if (fiter != m_map_face.end())
+			{
+				m_map_face.erase(fiter);
+			}
+			m_faces.remove(h->face());
+			//remove edge between vp[0] and vp[1]
+			m_edges.remove(h->edge());
+			delete h->edge();
+			delete h->sym();
+			delete h;
+			
+		}
+		else //if vp[0] vp[1] don't share an edge vp[1] must be on boundary
+		{
+			H * hvp1 = vp[1]->halfedge();//most ccwly in-coming halfedge must be on boundary
+			H * hvp1_next = hvp1->he_next()->he_sym();
+			//m_edges
+			if (hvp1->source()->id() < vp[0]->id())
+				vp[0]->edges().insert(hvp1->source());
+			//halfedges
+			hvp1->target() = vp[0];
+			while (hvp1_next != NULL)
+			{
+				if (hvp1_next->source()->id() < vp[0]->id())
+					vp[0]->edges().insert(hvp1_next->source());
+				hvp1_next->target() = vp[0];
+				if (hvp1_next->he_sym() != NULL)
+				{
+					hvp1_next->he_sym()->source() = vp[0];
+				}
+				hvp1_next = hvp1_next->he_next()->he_sym();
+			}
+			std::map<int, V*>::iterator viter = m_map_vert.find(vp[1]->id());
+			if (viter != m_map_vert.end())
+			{
+				m_map_vert.erase(viter);
+			}
+			m_verts.remove(vp[1]);//delete vp[1];
+		}
+	}
+	template<typename V, typename E, typename F, typename H>
+	void MyMesh<V, E, F, H>::QEM(double threshold, int times)
+	{
+		priority_queue<ValidPair * , vector<ValidPair *>, greater<ValidPair *>> heap;
+		for (MeshEdgeIterator eiter(this); !eiter.end(); ++eiter)//all valid pairs on edge
+		{
+			E * pE = *eiter;
+			V * v1 = pE->halfedge(0)->source();
+			V * v2 = pE->halfedge(1)->target();
+			ValidPair * vp = new ValidPair(v1, v2);
+			MatrixXd v = after_contraction(*vp);
+			vp->setCost(compute_cost(v, compute_error(v1) + compute_error(v2)));
+			heap.push(vp);
+		}
+
+		//to do: add valid pairs that are not on edge
+
+		//contraction of valid pairs with minimum cost for (times) times
+		for (int i = 0; i < times || !heap.empty(); ++i)
+		{
+			ValidPair * vp = heap.top();
+			contract_valid_pair(*vp);
+			heap.pop();
 		}
 	}
 }
